bb6c4fd4629706c851c05f2038b81a4090e46a3f
Anna Maria Lester <anna47c4@stud.kea.dk>
2023-05-22 21:12:43 +0200

    breadcrumbs removed from index + giftc page done

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -28,13 +28,16 @@ function Breadcrumbs() {
 }
 
 export default Breadcrumbs;
- */
-import { Link, useLocation } from "react-router-dom";
-
-function Breadcrumbs() {
   //Funktion der sørger for at vi får vist brugerens lokation på siden, og
   //sørger for at Produkter ikke er klikbar, men blot vises når man er inde på
   //f.eks. vinyler vil der stå Produkter > Vinyler
+     //Her i vores return statement' får vi det gældende resultat ud,
+      //som vi ønsker at vise i vores frontend
+ */
+/* mport { Link, useLocation } from "react-router-dom";
+
+function Breadcrumbs() {
+
   const location = useLocation();
   let currentLink = "";
 
@@ -51,8 +54,46 @@ function Breadcrumbs() {
           </div>
         );
       }
-      //Her i vores return statement' får vi det gældende resultat ud,
-      //som vi ønsker at vise i vores frontend
+   
+      return (
+        <div className="crumb" key={crumb}>
+          <Link to={currentLink} className="krumme">
+            {crumb}
+          </Link>
+        </div>
+      );
+    });
+
+  return <div className="breadcrumbs">{crumbs}</div>;
+}
+
+export default Breadcrumbs;
+ */
+import { Link, useLocation } from "react-router-dom";
+
+function Breadcrumbs() {
+  const location = useLocation();
+  let currentLink = "";
+
+  // Tjek om placeringen er root-stien ("/")
+  if (location.pathname === "/") {
+    return null; // Returner intet (ingen brødkrummer)
+  }
+
+  const crumbs = location.pathname
+    .split("/")
+    .filter((crumb) => crumb !== "")
+    .map((crumb, index) => {
+      currentLink += `/${crumb}`;
+
+      if (index === 0) {
+        return (
+          <div className="crumb" key={crumb}>
+            {crumb}
+          </div>
+        );
+      }
+
       return (
         <div className="crumb" key={crumb}>
           <Link to={currentLink} className="krumme">
--- src/pages/Gavekort.jsx
+++ src/pages/Gavekort.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { useState } from "react";
+import Button from "../components/Button";
+function GiftCardForm() {
+  const [amount, setAmount] = useState(0);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-function Gavekort() {
-  return <div>Gavekort</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Her kan du implementere den nødvendige logik for at behandle formulardataen, f.eks. sende den til en server eller udløse en handling
+    console.log(
+      "Gavekort beløb:",
+      amount,
+      "Købers navn:",
+      name,
+      "Købers email:",
+      email
+    );
+    // Nulstil formularfelterne efter indsendelse
+    setAmount(0);
+    setName("");
+    setEmail("");
+  };
+
+  return (
+    <div className="gift-card-form">
+      <h2>Køb et gavekort</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="amount">Gavekortbeløb i DKK:</label>
+          <input
+            type="text"
+            id="amount"
+            value={amount.toString()}
+            onChange={(e) => setAmount(parseInt(e.target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Navn:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <Button
+          desc={"KØB GAVEKORT"}
+          className={"cta-btn"}
+          clickAction={handleSubmit}
+          type="submit"
+        ></Button>
+      </form>
+    </div>
+  );
 }
 
-export default Gavekort;
+export default GiftCardForm;
